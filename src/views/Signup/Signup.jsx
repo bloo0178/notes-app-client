3f758ef8653aa035106d13e7b954d8b851882b92
Jeff Bloom <bloo0178@gmail.com>
2019-06-12 10:36:26 -0400

    Connected signup to Cognito. See comments in Signup.jsx for future improvements.

--- src/views/Signup/Signup.jsx
+++ src/views/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import LoaderButton from "../../components/LoaderButton/LoaderButton";
 import styles from "./Signup.module.css";
+import { Auth } from "aws-amplify";
 
 class Signup extends Component {
 	state = {
@@ -33,13 +34,46 @@ class Signup extends Component {
 	handleSubmit = async event => {
 		event.preventDefault();
 		this.setState({ isLoading: true });
-		this.setState({ newUser: "test" });
+
+		try {
+			const newUser = await Auth.signUp({
+				username: this.state.email,
+				password: this.state.password
+            });
+            // User won't currently be able to confirm account if 
+            // they refresh page at confirm step. They will need to
+            // create a new account. Need to:
+            // 1 - Check for UsernameExistsException in the catch block here
+            // 2 - Use Auth.resendSignUp() to resend the code if user was not
+            // previously confirmed.
+            // 3 - Confirm the code as normal
+            // https://aws-amplify.github.io/amplify-js/api/classes/authclass.html
+			this.setState({
+				newUser
+			});
+		} catch (e) {
+			alert(e.message);
+		}
 		this.setState({ isLoading: false });
 	};
 
 	handleConfirmationSubmit = async event => {
 		event.preventDefault();
 		this.setState({ isLoading: true });
+
+		try {
+			await Auth.confirmSignUp(
+				this.state.email,
+				this.state.confirmationCode
+			);
+			await Auth.signIn(this.state.email, this.state.password);
+
+			this.props.userHasAuthenticated(true);
+			this.props.history.push("/");
+		} catch (e) {
+			alert(e.message);
+			this.setState({ isLoading: false });
+		}
 	};
 
 	renderConfirmationForm() {
@@ -47,7 +81,7 @@ class Signup extends Component {
 			<form onSubmit={this.handleConfirmationSubmit}>
 				<label>Confirmation Code</label>
 				<input
-                    id="confirmationCode"
+					id="confirmationCode"
 					autoFocus
 					type="tel"
 					value={this.state.confirmationCode}
@@ -70,22 +104,22 @@ class Signup extends Component {
 			<form onSubmit={this.handleSubmit}>
 				<label>Email</label>
 				<input
-                    autoFocus
-                    id="email"
+					autoFocus
+					id="email"
 					type="email"
 					value={this.state.email}
 					onChange={this.handleChange}
 				/>
 				<label>Password</label>
 				<input
-                    id="password"
+					id="password"
 					value={this.state.password}
 					onChange={this.handleChange}
 					type="password"
 				/>
 				<label>Confirm Password</label>
 				<input
-                    id="confirmPassword"
+					id="confirmPassword"
 					value={this.state.confirmPassword}
 					onChange={this.handleChange}
 					type="password"
