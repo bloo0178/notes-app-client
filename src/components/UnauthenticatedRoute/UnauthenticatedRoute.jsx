23c85ab91565e04868b4121710a50951ce87ebb3
Jeff Bloom <bloo0178@gmail.com>
2019-06-14 18:23:04 -0400

    Added UnauthenticatedRoute component.

---
+++ src/components/UnauthenticatedRoute/UnauthenticatedRoute.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export const UnauthenticatedRoute = ({
+	component: C,
+	props: cProps,
+	...rest
+}) => (
+	<Route
+		{...rest}
+		render={props =>
+			!cProps.isAuthenticated ? (
+				<C {...props} {...cProps} />
+			) : (
+				<Redirect to="/" />
+			)
+		}
+	/>
+);
